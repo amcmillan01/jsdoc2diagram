47ca310753780f21e82f9cc4311a7882dd1a2178
Andrei McMillan <amcmillan@yieldbot.com>
2015-10-24 01:56:40 -0400

    add example file

---
+++ example/index.js
@@ -0,0 +1,46 @@
+/**
+ * @constructor
+ */
+var Garage = function(){
+
+};
+
+/**
+ * @return {number}
+ */
+Garage.prototype.getVehicleCount = function(){
+
+};
+
+
+/**
+ *
+ * @constructor
+ * @memberof Garage
+ */
+var Car = function(){
+  /**
+   *
+   * @type {string}
+   */
+  this.name = '';
+  /**
+   *
+   * @type {string}
+   */
+  this.color = '';
+};
+
+/**
+ * @return {boolean}
+ */
+Car.prototype.isOn = function(){
+
+};
+
+/**
+ * @return {boolean}
+ */
+Car.prototype.hasNavigation = function(){
+
+};
